fdb348dac4f86089959fba487d6498bddc4f832f
Cesar Escalante <39380567+CesarEscalante@users.noreply.github.com>
2025-12-18 13:32:59 -0600

    cambio para enrutamientos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CrearGrupo from "./pages/CrearGrupo";
 import Grupo from "./pages/Grupo";
 import Revelar from "./pages/Revelar";
@@ -6,13 +6,11 @@ import Revelar from "./pages/Revelar";
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<CrearGrupo />} />
           <Route path="/grupo/:id" element={<Grupo />} />
           <Route path="/revelar/:token" element={<Revelar />} />
         </Routes>
-      </BrowserRouter>
     </div>
 
   );
